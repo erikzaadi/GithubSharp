106bf4227ee98a492de8c7481a444b50fda0a2bf
Erik Zaadi <erik.zaadi@gmail.com>
2011-05-05 16:35:20 +0300

    continuing to update mvc sample

--- Samples/MvcSample/MvcApplication/Models/HtmlHelpers/AdditionalHtmlAndUrlHelpers.cs
+++ Samples/MvcSample/MvcApplication/Models/HtmlHelpers/AdditionalHtmlAndUrlHelpers.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Text;
+using System.Web.WebPages;
 
 namespace GithubSharp.Samples.MvcSample.MvcApplication.Models.HtmlHelpers
 {
--- Samples/MvcSample/MvcUI/Views/Shared/Footer.ascx
+++
@@ -1,2 +0,0 @@
-<%@ Control Language="C#" Inherits="System.Web.Mvc.ViewUserControl" %>
-Copyright 2011 <a href"http://erikzaadi.com">Erik Zaadi</a>
